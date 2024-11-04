dcaa3a73da7d3aca5af043304a24f52432113c03
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-04 13:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ Object.values(obj).forEach(([symbol, value])=>{
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
-
+console.log(convertToRoman(67894));   
+console.log(convertToRoman(49879));   
+console.log(convertToRoman(12345)); 
 
 
 
