8265bdec67d7363d76a4e41b91f9f4ca82f13607
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-04 13:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,16 +20,14 @@ function convertToRoman(num) {
 Object.values(obj).forEach(([symbol, value])=>{
 	let count = Math.floor(num / value);
 	result = result + symbol.repeat(count);
-	num = num - value * count;
+	num = num - (value * count);
 })
 	return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log(convertToRoman(36));
-console.log(convertToRoman(67894));   
-console.log(convertToRoman(49879));   
-console.log(convertToRoman(12345)); 
+
 
 
 
